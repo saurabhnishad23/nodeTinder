5e6a18a08776e657d7619e6977dc75d6ce8b49f9
Saurabh <saurabhnishad@hotmail.com>
2024-11-07 15:19:49 +0530

    exploring get, post, delete api

--- src/app.js
+++ src/app.js
@@ -1,15 +1,26 @@
 const express = require("express");
 
 const app = express();
+// order of routing path matter. it follow like try catch error method
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Saurabh", lastName: "Nishad" });
+});
 
-app.use("/hello", (req, res) => {
-  res.send("Hello Node1");
+app.post("/user", (req, res) => {
+  res.send("Data saved Successfully");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("user data deleted");
 });
 
 app.use("/test", (req, res) => {
   res.send("Testing the server");
 });
 
+app.use("/", (req, res) => {
+  res.send("Welcome to Node Project");
+});
 app.listen(3000, () => {
   //   res.send("Hello");
   console.log("Server Started");
